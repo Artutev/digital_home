edafedfc69c06c071cfc6a21176a1968b71a0ab4
Artutev <artutev@gmail.com>
2024-06-06 12:53:50 +0500

    first commit

---
+++ resumeApp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Projects)
+admin.site.register(ContactInfo)
+admin.site.register(Resume)
+admin.site.register(MyPhoto)
+admin.site.register(Message)
+admin.site.register(About)
+
+
